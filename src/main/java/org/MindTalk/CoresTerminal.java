641cc590a92a7def059f0ae4f6bb81c8d65acd65
Pedro Medeiros das Chagas <pmdcpe@gmail.com>
2025-02-13 17:41:09 -0300

    feat: criando a classe de cores.

---
+++ src/main/java/org/MindTalk/CoresTerminal.java
@@ -0,0 +1,21 @@
+package org.MindTalk;
+
+public class CoresTerminal {
+    public static final String RESET = "\u001B[0m";
+    public static final String VERMELHO = "\u001B[31m";
+    public static final String VERDE = "\u001B[32m";
+    public static final String AMARELO = "\u001B[33m";
+    public static final String AZUL = "\u001B[34m";
+    public static final String ROXO = "\u001B[35m";
+    public static final String CIANO = "\u001B[36m";
+    public static final String BOLD = "\u001B[1m";
+
+    public static final String BG_VERMELHO = "\u001B[41m";
+    public static final String BG_VERDE = "\u001B[42m";
+    public static final String BG_AMARELO = "\u001B[43m";
+    public static final String BG_AZUL = "\u001B[44m";
+    public static final String BG_ROXO = "\u001B[45m";
+    public static final String BG_CIANO = "\u001B[46m";
+    public static final String BG_BRANCO = "\u001B[47m";
+
+}
