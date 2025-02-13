8886b8270115de6c19339dd8c97157e30ea87b50
Pedro Medeiros das Chagas <pmdcpe@gmail.com>
2025-02-13 17:36:08 -0300

    feat: adicionando funções à Façade PacienteObserver.

--- src/main/java/org/MindTalk/PacienteObserver.java
+++ src/main/java/org/MindTalk/PacienteObserver.java
@@ -1,18 +1,64 @@
 package org.MindTalk;
 
-public class PacienteObserver extends Usuario{
-    private String cpf;
+import java.io.FileNotFoundException;
+import java.util.List;
 
-    public PacienteObserver(String nome, String cpf) {
-        super(nome);
-        this.cpf = cpf;
+class SistemaFacade {
+    private DatabaseSingleton banco = DatabaseSingleton.getInstance();
+
+    public PsicologoSubject cadastrarPsicologo(String nome, String crp) {
+        FactoryUsuario factoryPsicologo = new FactoryPsicologo();
+        PsicologoSubject psicologo = (PsicologoSubject) factoryPsicologo.criarUsuario(nome, crp);
+        banco.salvarPsicologo("Psicologo: " + nome + " | CRP: " + crp);
+        return psicologo;
     }
 
-    public void receberNotificacao(String mensagem) {
-        System.out.println("📢 Paciente " + nome + " recebeu notificação: " + mensagem);
+    public PacienteObserver cadastrarPaciente(String nome, String cpf,PsicologoSubject psicologo) {
+        FactoryUsuario factoryPaciente = new FactoryPaciente();
+        PacienteObserver paciente = (PacienteObserver) factoryPaciente.criarUsuario(nome, cpf);
+        String dadosPaciente = "Paciente: " + nome + " | CPF: " + cpf;
+        String dadosPsicologo = "Psicologo: " + psicologo.getNome() + " | CRP: " + psicologo.getCrp();
+        banco.salvarPaciente(dadosPaciente, dadosPsicologo);
+        return paciente;
     }
 
-    public String getCpf() {
-        return cpf;
+    public void criarSalaVirtual(String nomeSalaVirtual, PsicologoSubject psicologo, PacienteObserver paciente) {
+        WearableAdapter wearable = new WearableAdapter(new WearableDevice());
+        SalaVirtual sala = new SalaVirtual(nomeSalaVirtual, psicologo, paciente, wearable);
+
+        banco.salvarDados("Sala: " + nomeSalaVirtual + " | Psicologo: " + psicologo.getNome() + " | Paciente: " + paciente.getNome());
+
+        psicologo.notificarPaciente("🎥Sua consulta na " + nomeSalaVirtual + " iniciou, seja bem-vindo(a).");
+        sala.iniciarAtendimento();
     }
+
+    public PsicologoSubject realizarLogin(String Nome, String CRP) throws FileNotFoundException {
+        if(banco.pesquisarPsicologo(Nome, CRP)){
+            FactoryUsuario factoryPsicologo = new FactoryPsicologo();
+            return (PsicologoSubject) factoryPsicologo.criarUsuario(Nome, CRP);
+        }else{
+            return null;
+        }
+    }
+
+
+    public boolean pesquisarPsicologo(String nome, String crp) throws FileNotFoundException {
+        return banco.pesquisarPsicologo(nome, crp);
+    }
+
+    public void exibirListaPacientes(PsicologoSubject psicologo) {
+        try {
+            List<String> pacientes = banco.getListaPacientes(psicologo);
+            for (String paciente : pacientes) {
+                System.out.println(CoresTerminal.BOLD + paciente + CoresTerminal.RESET);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public PacienteObserver pesquisarPaciente(String nome, String cpf, PsicologoSubject psicologo) throws FileNotFoundException {
+        return banco.pesquisarPaciente(nome, cpf, psicologo);
+    }
+
 }
