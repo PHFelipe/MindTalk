cba5b38c7134b7f51a06a50a2f710ddff0d529d0
Pedro Medeiros das Chagas <pmdcpe@gmail.com>
2025-02-13 17:38:06 -0300

    feat: adicionando cores à façade principal.

--- src/main/java/org/MindTalk/MindTalkMenu.java
+++ src/main/java/org/MindTalk/MindTalkMenu.java
@@ -1,18 +1,21 @@
 package org.MindTalk;
+import org.MindTalk.CoresTerminal;
+
 
 public class MindTalkMenu {
     public static void exibirMenuInicial() {
         System.out.println("\n===== MindTalk - Menu Inicial =====");
-        System.out.println("1️⃣ Cadastrar Psicólogo");
-        System.out.println("2️⃣ Realizar Login");
-        System.out.println("0️⃣ Sair");
+        System.out.println(CoresTerminal.AZUL + "1 - Cadastrar Psicólogo" );
+        System.out.println("2 - Realizar Login" );
+        System.out.println("0 - Sair" + CoresTerminal.RESET);
         System.out.print("Escolha uma opção: ");
     }
 
     public static void exibirMenuPrincipal(){
         System.out.println("\n===== MindTalk - Menu Principal =====");
-        System.out.println("1️⃣ Iniciar Sala Virtual");
-        System.out.println("0️⃣ Sair");
+        System.out.println(CoresTerminal.AZUL + "1 - Iniciar Sala Virtual");
+        System.out.println("2 - Listar Pacientes");
+        System.out.println("0 - Sair" + CoresTerminal.RESET);
         System.out.print("Escolha uma opção: ");
     }
 
